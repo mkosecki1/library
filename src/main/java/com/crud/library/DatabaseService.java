61a1d9615eb8972b786ed84803bd757252314a2b
“mkosecki1 <mkosecki@op.pl>
2018-04-25 18:38:56 +0200

    2 commit

---
+++ src/main/java/com/crud/library/DatabaseService.java
@@ -0,0 +1,7 @@
+package com.crud.library;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+}
--- src/main/java/com/crud/library/LibraryController.java
+++ src/main/java/com/crud/library/LibraryController.java
@@ -1,9 +1,35 @@
 package com.crud.library;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("ver1/library")
 @RestController
 public class LibraryController {
+    @RequestMapping(method = RequestMethod.POST, value = "addBook")
+    public void addBook(){
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteBook")
+    public void deleteBook(){
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getBook")
+    public BookDto getBook(){
+        return null;
+    }
+
+    @RequestMapping()
+    public List<BookDto> getBooks(){
+        return null;
+    }
+
+    @RequestMapping()
+    public BookDto modificationBook(@RequestBody BookDto bookId){
+        return null;
+    }
+
 
 
 }
